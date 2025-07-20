59caf0444ea80fedb1246ff0beca903c91ed8c8d
sunland <Liuxizekali@outlook.com>
2025-07-20 09:04:15 +0800

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-// Supabase 项目配置信息
-const SUPABASE_URL = 'https://klyrasrqgxijwrxuoevj.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtseXJhc3JxZ3hpandyeHVvZXZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4ODUyMzcsImV4cCI6MjA2ODQ2MTIzN30.qjeTrLp_QquSwvF09HrrQd-stPtgu6H51-Zdb4JUeSM'
-
-// 创建并导出 Supabase 客户端
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
